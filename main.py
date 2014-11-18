4bd36b5d9cd842ebeca14306ff6b0d482a7a2db4
Pablo Varela <pablovarela182@gmail.com>
2014-11-18 18:12:59 +0100

    Estructura general del algoritmo realizada, solo falta el criterio de parada cuando se generan todas las soluciones posibles, porque ahora es un bucle infinito

--- main.py
+++ main.py
@@ -5,19 +5,28 @@ import random
 n=10 # Numero de ciudades
 distancias={} # diccionario para guardar las distancias entre las ciudades cargado del fichero
 
-# imprime las distancias (del diccionario) entre las ciudades cuyos indices estan juntos en el array 'indices'
-def imprimirDistancias(indices, diccionario):
-	i=0
+# calcular la distancia total
+def calcularDistancia(indices):
+	i=0; total=0
+	# sumo al total la distancia de 0 a 1
+	aux = distancias[1]
+	total += int(aux[0])
+	# sumo al total la distancia de 0 a n-1
+	aux = distancias[n-1]
+	total += int(aux[0])
+
 	for i in range(len(indices)-1):
 		aux = indices[i]
 		aux1 = indices[i+1]
 		if aux1 < aux:
-			array = diccionario[aux]
+			array = distancias[aux]
 			d = int(array[aux1])
 		elif aux1 > aux:
-			array = diccionario[aux1]
+			array = distancias[aux1]
 			d = int(array[aux])
-		print "Distancia entre %d y %d: %d" % (aux,aux1,d)
+		# print "Distancia entre %d y %d: %d" % (aux,aux1,d)
+		total += d
+	return total
 
 # lectura del fichero y carga de datos
 def leerfichero():
@@ -32,26 +41,52 @@ def leerfichero():
 	# print distancias
 	f.close()
 
+# intercambiar dos posiciones aleatorias
+def generar(lista):
+	a=0; b=0
+	otralista = lista[:]
+	# dos indices aleatorios
+	while a == b:
+		a = random.randrange(0,n-1)
+		b = random.randrange(0,n-1)
+	# intercambio dos posiciones aleatorias
+	aux = otralista[a]
+	otralista[a] = otralista[b]
+	otralista[b] = aux
+
+	print otralista
+	return otralista
+
 # funcion principal
 def main():
 
 	leerfichero() # cargo los datos
-	posiciones = range(1,n) # array de posiciones de las ciudades
-	a=0; b=0 # indices a cambiar 
+	vecinoactual = range(1,n) # array de posiciones de las ciudades
+	mejorvecino = []
 
-	while a == b: # son aleatorios pero no pueden ser el mismo (porque no se cambiarian)
-		a = random.randrange(0,n-1)
-		b = random.randrange(0,n-1)
+	# Posiciones aleatorias
+	random.shuffle(vecinoactual)
+	print vecinoactual
+	total = calcularDistancia(vecinoactual)
+	print "Distancia total: %d" % total
 
-	# intercambio los indices a y b
-	aux = posiciones[a]
-	posiciones[a] = posiciones[b]	
-	posiciones[b] = aux
+	while True:
 
-	# solucionInicial = [a,b]
-	print "Solucion inicial: intercambiar %d con %d" % (a,b)
-	print posiciones
-	imprimirDistancias(posiciones,distancias)
+		mejorvecino = vecinoactual[:] # copio el vecino actual
+		aux = []
+
+		while True:
+			aux = generar(vecinoactual)
+			if calcularDistancia(aux) < calcularDistancia(mejorvecino):
+				break
+
+		if calcularDistancia(aux) < calcularDistancia(vecinoactual):
+			vecinoactual = aux[:]
+
+		print "Distancia actual: %d" % calcularDistancia(mejorvecino)
+
+		if (calcularDistancia(vecinoactual) >= calcularDistancia(mejorvecino)):
+			break
 
 if __name__ == '__main__': # funcion main en el archivo main
 	main()
